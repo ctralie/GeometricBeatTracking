9949737e6e8a3c4eef59881ea7e020089af8273d
Chris Tralie <chris.tralie@gmail.com>
2016-01-18 16:17:05 -0500

    Working on doing laplacian coordinates in small blocks and merging the results.  Needs more work and testing

--- CircularCoordinates.py
+++ CircularCoordinates.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.sparse
+import scipy.stats
 import scipy.sparse.linalg as slinalg
 import matplotlib.pyplot as plt
 import prox_tv as ptv
@@ -9,6 +10,7 @@ from SoundTools import *
 
 EIG1 = 1
 EIG2 = 2
+VALIDTEMPOMIN = 0.1
 
 def SSMToBinary(D, Kappa):
     N = D.shape[0]
@@ -94,14 +96,25 @@ def plotCircularCoordinates(s, D, v, theta, onsets, gtOnsets):
     plt.title('Circular Coordinates')
     plt.show()
 
-def getCircularCoordinates(s, W, NPCs, Normalize = True):
-    NEig = 16
-    (U, S) = s.getSlidingWindowLeftSVD(W)
-    SMat = np.eye(NPCs)
-    np.fill_diagonal(SMat, S[0:NPCs])
-    V = s.getSlidingWindowRightSVD(W, NPCs)
-    X = U[:, 0:NPCs].dot(SMat).dot(V)
+def plotCircularCoordinates2(s, theta, onsets, gtOnsets):
+    plt.subplot(311)
+    plt.plot(s.novFn)
+    plt.title("Original Function")
+    
+    plt.subplot(312)
+    plt.plot(theta % 2*np.pi)
+    plt.title('Circular Coordinates')
 
+    plt.subplot(313)
+    plt.stem(onsets, np.ones(len(onsets)), 'b')
+    plt.hold(True)
+    if len(gtOnsets) > 0:
+        plt.stem(gtOnsets, 0.5*np.ones(len(gtOnsets)), 'r')
+    plt.title('Onsets')
+    plt.show()
+
+def getCircularCoordinatesBlock(X, Normalize = True):
+    NEig = 16
     #Compute SSM
     XSum = np.sum(X**2, 0)
     if Normalize:
@@ -110,8 +123,8 @@ def getCircularCoordinates(s, W, NPCs, Normalize = True):
     D = XSum[:, None] + XSum[None, :] - 2*(X.T.dot(X))
     
     #Compute spectral decomposition
-    #A = SSMToBinaryMutual(D, 0.02)
-    A = np.exp(-D*10)
+    A = SSMToBinaryMutual(D, 0.1)
+    #A = np.exp(-D*10)
     A[range(1, D.shape[0]), range(D.shape[0]-1)] = 1
     A[range(D.shape[0]-1), range(1, D.shape[0])] = 1
     #plotAdjacencyEdgesPCA(D, A, s)
@@ -126,7 +139,7 @@ def getCircularCoordinates(s, W, NPCs, Normalize = True):
         #http://stackoverflow.com/questions/12125952/scipys-sparse-eigsh-for-small-eigenvalues
         tic = time.time()
         w, v = slinalg.eigsh(L, k=NEig, sigma = 0, which = 'LM')
-        plotEigenvectors(v, NEig)
+        #plotEigenvectors(v, NEig)
         toc = time.time()
         print "Time computing eigenvectors: ", toc-tic
     except Exception as err:
@@ -139,6 +152,92 @@ def getCircularCoordinates(s, W, NPCs, Normalize = True):
         theta = -theta
     theta = theta - theta[0]
     return (D, L, v, theta)
+
+#Throw away blocks that have a very low slope after linear regression
+def discardBadBlocks(s, idxs, BlockAngles):
+    A = np.ones((len(idxs[0]), 2))
+    validIdx = []
+    for i in range(len(idxs)):
+        A[:, 0] = idxs[i]
+        m, c = np.linalg.lstsq(A, np.array(BlockAngles[i]))[0]
+        tempo = m*(s.Fs/float(s.hopSize))/(2*np.pi) #Convert to cycles per second
+        if tempo > VALIDTEMPOMIN:
+            validIdx.append(i)
+    idxs = [idxs[i] for i in validIdx]
+    BlockAngles = [BlockAngles[i] for i in validIdx]
+    return (idxs, BlockAngles)
+
+def medianAlignBlocks(idxs, BlockAngles):
+    N = len(idxs[0])
+    for i in range(1, len(idxs)):
+        k = 0
+        #Find the point where these sequences overlap
+        while k < N:
+            #TODO: Could do this faster with binary search
+            if idxs[i-1][k] == idxs[i][0]:
+                break
+            k += 1
+        if k == N:
+            print "ERROR: Blocks at incides %i and %i don't overlap"%(i-1, i)
+        else:
+            med1 = np.median(BlockAngles[i-1][k::])
+            med2 = np.median(BlockAngles[i][0:N-k])
+            BlockAngles[i] += (med1-med2)
+
+def medianMergeBlocks(idxs, BlockAngles, N, BlockLen, BlockHop):
+    theta = np.zeros((BlockLen/BlockHop, N))
+    currIdx = np.zeros(N, dtype=np.int64)
+    for i in range(len(idxs)):
+        theta[currIdx[idxs[i]], idxs[i]] = BlockAngles[i]
+        currIdx[idxs[i]] += 1
+    return scipy.stats.nanmedian(theta, 0)
+
+#Do circular coordinates in a sliding window and aggregate the results
+#in a consistent way
+def getCircularCoordinatesBlocks(s, W, NPCs, BlockLen, BlockHop, Normalize = True, doPlot = True):
+    #Step 0: Perform PCA on a sliding window over the entire song
+    (U, S) = s.getSlidingWindowLeftSVD(W)
+    SMat = np.eye(NPCs)
+    np.fill_diagonal(SMat, S[0:NPCs])
+    V = s.getSlidingWindowRightSVD(W, NPCs)
+    X = U[:, 0:NPCs].dot(SMat).dot(V)
+    Ds = []
+    Ls = []
+    vs = []
+    idxs = []
+    N = X.shape[1]
+    NBlocks = int(np.ceil(1 + (N - BlockLen)/BlockHop))
+    print "NBlocks = ", NBlocks
+    BlockAngles = [] #Holds the circular coordinates for each different block
+    #Step 1: Get the circular coordinates in blocks
+    for i in range(NBlocks):
+        thisidxs = np.arange(i*BlockHop, i*BlockHop+BlockLen, dtype=np.int64)
+        thisidxs = thisidxs[thisidxs < N]
+        idxs.append(thisidxs)
+    for i in range(NBlocks):
+        (D, L, v, theta) = getCircularCoordinatesBlock(X[:, idxs[i]], Normalize = True)
+        BlockAngles.append(theta)
+        Ds.append(D)
+        Ls.append(L)
+        vs.append(v)
+    #Step 2: Discard bad blocks and plot the blocks before alignment
+    (idxs, BlockAngles) = discardBadBlocks(s, idxs, BlockAngles)
+    if doPlot:
+        plt.hold(True)
+        for i in range(len(idxs)):
+            plt.plot(idxs[i], BlockAngles[i])
+        plt.title("Blocks Before Alignment")
+        plt.show()
+    #Step 3: Median align the blocks and plot the result
+    medianAlignBlocks(idxs, BlockAngles)
+    if doPlot:
+        plt.hold(True)
+        for i in range(len(idxs)):
+            plt.plot(idxs[i], BlockAngles[i])
+        plt.title("Blocks After Alignment")
+        plt.show()
+    #Step 4: Finish merging all of the blocks by taking the median of values at the same time coordinate
+    return medianMergeBlocks(idxs, BlockAngles, N, BlockLen, BlockHop)
 
 #Give different angles a score based on the energy of the novelty function
 #that occurs around those angles
@@ -172,46 +271,56 @@ def getOnsetsPassingAngle(t, angle):
 if __name__ == "__main__":
     np.random.seed(100)
     T = 200
-    NPCs = 10
+    NPCs = 20
     noiseSigma = 0.05
     gaussSigma = 3
-    (gtPulses, x) = getSyntheticPulseTrainFreqAmpDrift(5000, T-30, T+30, 1, 1, noiseSigma, gaussSigma)
+    (gtPulses, x) = getSyntheticPulseTrainFreqAmpDrift(5000, T, T, 1, 1, noiseSigma, gaussSigma)
     #(gtPulses2, x2) = getSyntheticPulseTrainFreqAmpDrift(5000, T/2, T/2, 1, 1, 0, gaussSigma)
     #gtPulses += gtPulses2
     #x += 0.5*x2
+    gtOnsets = np.arange(len(gtPulses))
+    gtOnsets = gtOnsets[gtPulses > 0]
+    
     plt.plot(gtPulses)
     plt.show()
     x = x - np.mean(x)
     s = BeatingSound()
     s.novFn = x
+    s.Fs = 300
+    s.hopSize = 1
 
     W = 300
-    (D, L, v, theta) = getCircularCoordinates(s, W, NPCs)
-    plt.subplot(311)
-    plt.plot(theta)
-    plt.title('Original Theta')
-    theta2 = ptv.tv1_1d(theta, 1)
-    plt.subplot(312)
-    plt.plot(theta2)
-    plt.title('TV Theta')
-    plt.subplot(313)
-    plt.plot(theta2 - theta)
-    plt.title('Difference')
-    plt.show()
-    theta = theta2
-    
+    theta = getCircularCoordinatesBlocks(s, W, NPCs, 600, 100)
     (angles, scores, scoresFinal) = scoreAngles(s, theta, 1000)
     transitionAngle = angles[np.argmax(scoresFinal)]
     
     print "transitionAngle = ", transitionAngle*180/np.pi
     onsets = getOnsetsPassingAngle(theta, transitionAngle)
-    gtOnsets = np.arange(len(gtPulses))
-    gtOnsets = gtOnsets[gtPulses > 0]
+    plotCircularCoordinates2(s, theta, onsets, gtOnsets)
     
-#    plt.subplot(121)
-#    plt.plot(np.arange(len(theta)), 180*(theta%2*np.pi)/np.pi, 'b')
-#    plt.hold(True)
-#    plt.plot([0, len(theta)], [transitionAngle*180/np.pi, transitionAngle*180/np.pi], 'r')
-    
-    plotCircularCoordinates(s, D, v, theta, onsets, gtOnsets)
-    
+#    (D, L, v, theta) = getCircularCoordinates(s, W, NPCs)
+#    plt.subplot(311)
+#    plt.plot(theta)
+#    plt.title('Original Theta')
+#    theta2 = ptv.tv1_1d(theta, 1)
+#    plt.subplot(312)
+#    plt.plot(theta2)
+#    plt.title('TV Theta')
+#    plt.subplot(313)
+#    plt.plot(theta2 - theta)
+#    plt.title('Difference')
+#    plt.show()
+#    theta = theta2
+#    
+#    (angles, scores, scoresFinal) = scoreAngles(s, theta, 1000)
+#    transitionAngle = angles[np.argmax(scoresFinal)]
+#    
+#    print "transitionAngle = ", transitionAngle*180/np.pi
+#    onsets = getOnsetsPassingAngle(theta, transitionAngle)
+#    
+##    plt.subplot(121)
+##    plt.plot(np.arange(len(theta)), 180*(theta%2*np.pi)/np.pi, 'b')
+##    plt.hold(True)
+##    plt.plot([0, len(theta)], [transitionAngle*180/np.pi, transitionAngle*180/np.pi], 'r')
+#    
+#    plotCircularCoordinates(s, D, v, theta, onsets, gtOnsets)
